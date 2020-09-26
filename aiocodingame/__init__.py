0b0d914eaf4d0985360e029228b3930ec8101a1a
Takos22 <tora.akos@gmail.com>
2020-09-26 19:38:54 +0200

    Version fix

--- aiocodingame/__init__.py
+++ aiocodingame/__init__.py
@@ -9,11 +9,11 @@ from typing import NamedTuple
 
 VersionInfo = NamedTuple("VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int)
 
-version_info = VersionInfo(major=0, minor=3, micro=2, releaselevel="post", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
 
 __title__ = "aiocodingame"
 __author__ = "takos22"
-__version__ = "0.3.2.post0"
+__version__ = "0.1.0a"
 
 __all__ = [
     "Client",
