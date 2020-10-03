3f0f61b2e065c63c2feff641b0800bd9095b5825
Takos22 <tora.akos@gmail.com>
2020-10-03 18:11:53 +0200

    v0.1.0

--- aiocodingame/__init__.py
+++ aiocodingame/__init__.py
@@ -9,11 +9,11 @@ from typing import NamedTuple
 
 VersionInfo = NamedTuple("VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int)
 
-version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="alpha", serial=0)
+version_info = VersionInfo(major=0, minor=1, micro=0, releaselevel="", serial=0)
 
 __title__ = "aiocodingame"
 __author__ = "takos22"
-__version__ = "0.1.0a"
+__version__ = "0.1.0"
 
 __all__ = [
     "Client",
