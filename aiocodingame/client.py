43cc5956dc6b85e20f818ea7c97c73354e8655b3
Takos22 <takos2210@gmail.com>
2020-12-10 21:46:12 +0100

    Formatting

--- aiocodingame/client.py
+++ aiocodingame/client.py
@@ -26,7 +26,8 @@ class Client:
             If the client is logged in as a CodinGamer.
 
         codingamer: Optional[:class:`CodinGamer`]
-            The CodinGamer that is logged in through the client. ``None`` if the client isn't logged in.
+            The CodinGamer that is logged in through the client. ``None`` if the
+            client isn't logged in.
     """
 
     _CODINGAMER_HANDLE_REGEX = re.compile(r"[0-9a-f]{32}[0-9]{7}")
@@ -66,7 +67,8 @@ class Client:
         Raises
         ------
             :exc:`ValueError`
-                Error with the login (empty email, empty password, wrong email format, incorrect password, etc).
+                Error with the login (empty email, empty password,
+                wrong email format, incorrect password, etc).
 
         Returns
         --------
